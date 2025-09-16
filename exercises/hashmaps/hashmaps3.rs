07bc766128e175c8cba77c02ab355d429c414d02
Yang-ZhiHang <2097853195@qq.com>
2025-09-16 16:29:10 +0800

    update: enums, hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -39,6 +37,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        for (name, scored, conceded) in [
+            (team_1_name, team_1_score, team_2_score),
+            (team_2_name, team_2_score, team_1_score),
+        ] {
+            scores
+                .entry(name)
+                .and_modify(|t| {
+                    (*t).goals_scored += scored;
+                    (*t).goals_conceded += conceded;
+                })
+                .or_insert(Team {
+                    goals_scored: scored,
+                    goals_conceded: conceded,
+                });
+        }
     }
     scores
 }
