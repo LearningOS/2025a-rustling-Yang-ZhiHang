b9b6b31efa65980ce81cd09f8e1f976c0cb1cb64
Yang-ZhiHang <2097853195@qq.com>
2025-09-26 00:09:44 +0800

    update: macros

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -3,14 +3,15 @@
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-fn main() {
-    my_macro!();
-}
-
+// Macro will fold in where the macro is invoked, so we need to define it
+// before we can use it.
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
+}
+
+fn main() {
+    hello();;
+    my_macro!();
 }
